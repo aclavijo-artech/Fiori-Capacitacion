ad032ca50f35586781e11ffb67649f677c9237a7
aclavijo-artech <aclavijo@artech-consulting.com>
2025-09-02 10:54:50 -0300

    :rocket: Add data binding and update view for Fourth Page

--- webapp/controller/FourthPage.controller.js
+++ webapp/controller/FourthPage.controller.js
@@ -1,10 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("clase3.controller.FourthPage", {       
         onInit() {
+            const oData = {
+                name: "Andrés",
+                lastName: "Clavijo"
+            }
+
+            this.getView().setModel(new JSONModel(oData), "localModel");
+            this.getView().byId("fourthPageTitle").setText("Modelo Local");
         },
     });
 });
