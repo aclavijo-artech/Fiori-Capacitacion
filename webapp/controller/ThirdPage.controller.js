f01270ed07bdc43f65d115ed2d17f9fbe9d7403d
aclavijo-artech <aclavijo@artech-consulting.com>
2025-09-02 10:50:14 -0300

    :rocket: Implement navigation to Fourth Page and update routing parameters

--- webapp/controller/ThirdPage.controller.js
+++ webapp/controller/ThirdPage.controller.js
@@ -9,9 +9,18 @@ sap.ui.define([
         },
 
         _onRouteMatched(oEvent) {
-            const sParam = oEvent.getParameter("arguments");
+            const sParam = oEvent.getParameter("arguments").param;
 
-            this.getView().byId("thirdPageTextParam").setText(`Parámetro: ${sParam["param"]}`);
+            this.getView().byId("thirdPageTextParam").setText(`Parámetro: ${sParam}`);
+            this.sParam = sParam;
+        },
+
+        ontoFourthPage() {
+            const oRouter = this.getOwnerComponent().getRouter();
+            
+            oRouter.navTo("RouteFourthPage", {
+                param: this.sParam
+            });
         }
     });
 });
