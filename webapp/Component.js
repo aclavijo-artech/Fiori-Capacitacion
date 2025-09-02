a679adc65f2e47144efd420c88080cde847082c1
aclavijo-artech <aclavijo@artech-consulting.com>
2025-09-01 21:12:59 -0300

    💥 Removed comments from the configuration files

--- webapp/Component.js
+++ webapp/Component.js
@@ -6,20 +6,17 @@ sap.ui.define([
 
     return UIComponent.extend("clase3.Component", {
         metadata: {
-            manifest: "json",  // Indica que la configuración del componente se encuentra en el archivo manifest.json
+            manifest: "json",
             interfaces: [
-                "sap.ui.core.IAsyncContentCreation"  // Permite la creación asíncrona del contenido del componente para optimizar el rendimiento
+                "sap.ui.core.IAsyncContentCreation"
             ]
         },
 
         init() {
-            // Llama al init original de UIComponent para asegurar que se inicialice correctamente toda la funcionalidad base
             UIComponent.prototype.init.apply(this, arguments);
 
-            // Crea y asigna el modelo de dispositivo (info de pantalla, sistema, etc.) para usar en la app
             this.setModel(models.createDeviceModel(), "device");
 
-            // Inicializa el router para habilitar la navegación entre vistas definidas en el manifest.json
             this.getRouter().initialize();
         }
     });
