3a5ea7caf98a357f470280bed1c72889244c2f63
Andres Felipe Clavijo Rojas <dca6550a-fb07-44f0-ad4c-33d711a33d94>
2025-09-29 13:12:55 +0000

    :sparkles: Init Project

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.andres.clase7.controller.App", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/andres/clase7/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
