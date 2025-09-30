3e5ed0e719f6034bf9822101a16045593f6d00ed
Fabian Diz <fdiz@artech-consulting.com>
2025-09-30 18:45:46 +0000

    Primer commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["tpcurso/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"tpcurso/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
