27812317622059cae711281f70a986e5fdc27fd8
Fabian Diz <fdiz@artech-consulting.com>
2025-10-05 07:26:28 +0000

    primer intento

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,50 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, JSONModel, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("tpcurso.controller.App", {
         onInit() {
-        }
+            // Crear un modelo local para los resultados
+            var oModel = new JSONModel();
+            this.getView().setModel(oModel, "local");
+        },
+
+         onSearch: function () {
+            debugger;
+            var oView = this.getView();
+            var oODataModel = this.getOwnerComponent().getModel();
+            var oLocalModel = oView.getModel("local");
+            var sEbeln = oView.byId("idEbeln").getValue();
+
+            var aFilters = [];
+
+            if (sEbeln) {
+                aFilters.push(new Filter("EBELN", FilterOperator.EQ, sEbeln));
+            }
+
+            // Invocar servicio OData 
+            oODataModel.read("/OrdenesCompra", {
+                 filters: aFilters,
+                success: function (oData) {
+                  // Cargar resultados en modelo local
+                  oLocalModel.setProperty("/results", oData.results);
+                    },
+                    error: function (oError) {
+                    sap.m.MessageToast.show("Error al obtener datos");
+                    }
+                });
+
+                      // Enlazar tabla al modelo local
+                var oTable = oView.byId("idTable");
+                oTable.setModel(oLocalModel);
+                oTable.bindItems({
+                        path: "local>/results",
+                        template: oTable.getBindingInfo("items").template
+                    });
+         }
     });
 });
